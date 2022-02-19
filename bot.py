9f161821ed4551f913dd15ec32d8912fed875312
Nick <nikita.nikiforov.2015@mail.ru>
2022-02-19 19:02:24 +0300

    v1.2. fixes and other shit

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-# telegram status bot v1.1 by @codingstorm (@yfimsky)
+# telegram status bot v1.2 by @codingstorm (@yfimsky)
 
 import time # i dunno why i put it here
 import pythonping # really need to make bot working
@@ -7,12 +7,11 @@ from datetime import datetime # useless shit but i have it l0l
 
 bot = telebot.TeleBot('') # put ur telegram bot token here
 
-ping = pythonping.ping('') # put ur url here
-latency = ping.rtt_avg_ms # pinging site here
-
 banList = [] # ban list that using in start command
 
 admin = [] # put ur id here
+
+channel = "" # put ur telegram channel here but before add the bot there and make him admin
 
 
 @bot.message_handler(commands=['start']) # start command use it everywhere coz its doesnt matter
@@ -20,14 +19,16 @@ def send_message(message):
     if message.from_user.id not in banList:
         if message.from_user.id in admin:
             bot.reply_to(message, 'Started.')
+            ping = pythonping.ping('api.telegram.org')  # put ur url here
+            latency = ping.rtt_avg_ms  # pinging site here
             while latency < 2000:
                 now = datetime.now() # u can delete this if u want
                 current_time = now.strftime("%H:%M:%S") # this too
-                bot.send_message(-1001517841488,
+                bot.send_message(channel,
                                  'Telegram ping is: ' + str(latency) + '\n' + 'Local time is: ' + current_time)
                 time.sleep(300)
             else:
-                bot.send_message(-1001517841488, 'Telegram or server is down') # lib thinking 2000 ping = dead
+                bot.send_message(channel, 'Telegram or server is down') # lib thinking 2000 ping = dead
         else:
             bot.reply_to(message, 'You can not use that bot.\n' + 'Bot was not made chatting.')
             bot.send_message(message.chat.id, str(message.from_user.id) + " was banned.")
@@ -44,7 +45,7 @@ def send_message(message):
 @bot.message_handler(commands=['stop']) # stop command useful thing
 def stop(message):
     if message.from_user.id in admin:
-        print(':(')
+        bot.reply_to(message, ':(')
         bot.stop_bot()
     else:
         pass
